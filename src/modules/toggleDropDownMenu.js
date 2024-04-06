ac4b519bb0b03d9218206d770314f40b9ad3e9b2
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-04-06 11:50:11 +0300

    Style the menu

--- src/modules/toggleDropDownMenu.js
+++ src/modules/toggleDropDownMenu.js
@@ -1,14 +1,7 @@
 const toggleDropDownMenu = (menuIcon, id) => {
   const dropDownMenuById = document.getElementById(`dropdown-menu-${id}`);
-  if (dropDownMenuById.classList.contains("hidden")) {
-    menuIcon.classList.remove("fa-chevron-down");
-    menuIcon.classList.add("fa-chevron-up");
-    dropDownMenuById.classList.remove("hidden");
-  } else {
-    menuIcon.classList.remove("fa-chevron-up");
-    menuIcon.classList.add("fa-chevron-down");
-    dropDownMenuById.classList.add("hidden");
-  }
+  menuIcon.classList.toggle("rotate");
+  dropDownMenuById.classList.toggle("active");
 };
 
 export default toggleDropDownMenu;
