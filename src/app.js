7214eaf39516363daec8b19601e75db210194907
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-04-07 12:30:00 +0300

    Add hide and show menu

--- src/app.js
+++ src/app.js
@@ -1,5 +1,25 @@
 import toggleDropDownMenu from "./modules/toggleDropDownMenu.js";
 
+let scrollPosition = null;
+
+const handleShowHideHeader = () => {
+  const header = document.querySelector(".header");
+
+  if (window.scrollY === 0) {
+    header.classList.remove("show");
+  }
+
+  if (window.scrollY > scrollPosition) {
+    header.classList.remove("show");
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+    header.classList.add("show");
+  }
+
+  scrollPosition = window.scrollY;
+};
+
 const app = () => {
   const menuIcons = document.querySelectorAll(".menu-icon");
   console.log(menuIcons);
@@ -10,6 +30,8 @@ const app = () => {
       toggleDropDownMenu(menuIcon, id);
     });
   });
+
+  window.addEventListener("scroll", handleShowHideHeader);
 };
 
 export default app;
